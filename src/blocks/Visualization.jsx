7f91e5dd15b534bce633b89aaf6743a2e89e5f7e
ivan_maleshko <183198079+imaleshko@users.noreply.github.com>
2026-03-18 02:56:41 +0200

    Implement visualization component

---
+++ src/blocks/Visualization.jsx
@@ -0,0 +1,34 @@
+import Plot from "react-plotly.js";
+
+const Visualization = ({ trajectory_data }) => {
+  const max_val = Math.max(
+    ...trajectory_data.x_data,
+    ...trajectory_data.y_data,
+  );
+
+  return (
+    <Plot
+      data={[
+        {
+          x: trajectory_data.x_data,
+          y: trajectory_data.y_data,
+          z: trajectory_data.z_data,
+          type: "scatter3d",
+          mode: "lines",
+        },
+      ]}
+      layout={{
+        width: 700,
+        height: 700,
+        scene: {
+          aspectmode: "cube",
+          xaxis: { range: [0, max_val] },
+          yaxis: { range: [0, max_val] },
+        },
+        title: { text: "Графік прямолінійного рівноприскореного руху" },
+      }}
+    />
+  );
+};
+
+export default Visualization;
